146016f9837d5216aa5707317ad0e202e1ff457f
akssnsk <akss@outlook.com>
2014-06-04 10:23:51 -0700

    New files added

---
+++ Agent/Agent.cpp
@@ -0,0 +1,14 @@
+// Agent.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int RunAgent1();
+
+int main(int argc, char* argv[])
+{
+    RunAgent1();
+
+    return 0;
+}
+
